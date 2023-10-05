bf06b2cbfdad0d1811f04c2d1bff55892ffa560a
Christopher Ryan Lindner <lindner@linprog1.cs.fsu.edu>
2023-10-05 13:00:04 -0400

    cleaned up stuff

--- src/shell.c
+++ src/shell.c
@@ -18,10 +18,6 @@ int main()
 {
 	int commandIndex = 0;	// Keeps track of which token indexes hold commands like "ls" "echo" "cd" etc.
 	int fileIndex = 0; 	//Keeps track of index of file
-	char *path = NULL;	// When a command token is located like "ls" this will hold its path like "/bin/ls"
-	char *argList[10];	// Holds array of c-string arguments that get passed into execv() function. Will look something like {"/bin/ls","-ls",NULL}
-	int argCount = 0;	// Increments whenever an argument is found. Resets to 0 when a pipe is encountered.
-	bool addArg = false;	// Turns true when an item needs to be added to the argument list.
 	char *input = "start";	// Holds user input typed into shell.
 	tokenlist *tokens;	// User input is broken into tokens.
 	int outfd;
@@ -34,7 +30,7 @@ while (strcmp(input, "exit")!= 0)
 	input = get_input(); 
 	tokens = get_tokens(input);
 	//argListofTokens = get_tokens(input); 
-//	print(tokens);
+	print(tokens);
 
 	// Loop thru tokens...
 	for (int i=0; i<tokens->size; i++)
@@ -44,18 +40,12 @@ while (strcmp(input, "exit")!= 0)
 		{
 			// Do path search to locate it on the OS.
 			tokens->items[0] = get_path_to_command(tokens->items[i]);
-			// This variable will now hold the path (if it was found)
-			// or it will hold NULL if no path was found.
 
 			if(tokens->items[0]==NULL)
 			{
 				printf("command not found.\n");
 				exit(0);
 			}
-
-			// The OS path to the command needs to be first argument in argument list.
-			// So we add that now.
-			//argList[0] = path; argCount++;
 		}
 
 		// Get first character of token...
@@ -64,18 +54,14 @@ while (strcmp(input, "exit")!= 0)
 			case '$' :
 				// Replace environment variable.
 				tokens->items[i] = replace_environment_variable(tokens->items[i]);
-				addArg = true;
 				break;
 			case '~' :
 				// Replace tilda with home folder.
 				tokens->items[i] = replace_tilda(tokens->items[i]);
-				addArg = true;
 				break;
 			case '-' :
-				addArg = true;
 				break;
 			case '/' :
-				addArg = true;
 				break;
 			case '|' :
 				// If we reach a "pipe" then the next token should be a command token.
@@ -84,27 +70,14 @@ while (strcmp(input, "exit")!= 0)
 				break;
 			case '>' :
 				//output redirection
-				addArg = true;
 				fileIndex = i +1;
-				
 				break;
 			case '<' :
 				//input redirection
-				addArg = true;
 				break;
 		}
-		// Add token to argument list.
-		//if (addArg) {argList[argCount] = tokens->items[i]; argCount++;}
-		//addArg = false;
 	}
-//	print(tokens);
-
-	// Print details before executing command.
-//	printf("%s","Executing command...");
-	//printf("%s\n",path);
-//	printf("%s\n","Using arguments... ");
-	//for (int i=0; i<argCount; i++) {printf("%s\n",argList[i]);}
-//	printf("%s\n","Here is the output from executing...");
+	print(tokens);
 
 	// Execute command.
 	__pid_t pid = fork();
@@ -112,7 +85,7 @@ while (strcmp(input, "exit")!= 0)
 	if (pid == 0) 
 	{
 		//outfd = dup(STDOUT_FILENO); //Duplication STDOUT
-		for(int i = 0; i < tokens->size; i++) printf("%s\n", tokens->items[i]);
+		//for(int i = 0; i < tokens->size; i++) printf("%s\n", tokens->items[i]);
 
 		tokens->items[1] = NULL;
 
@@ -168,12 +141,7 @@ void showPrompt()
 void print(tokenlist *tokens)
 {
 	printf("Tokenlist...\n");
-	for (int i=0; i<tokens->size; i++)
-	{
-		printf("%p",&tokens->items[i]);
-		printf(" has token ");
-		printf("%s\n",tokens->items[i]);
-	}
+	for (int i=0; i<tokens->size; i++) {printf("%s\n",tokens->items[i]);}
 }
 
 char* replace_environment_variable(char* token)
